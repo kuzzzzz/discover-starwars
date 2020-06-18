2866332efc0bb5f24a00613d07368e739d52a12e
Atumah Daniel <atumahdaniel@gmail.com>
2020-06-18 08:27:00 +0100

    re-organized code-base

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import People from './components/People'
 import "./App.css";
 
 function App() {
+  
+
   return (
-    <div className="topnav">
-      <div className="Logo"></div>
-      <div className="nav">
-        <ul>
-          <li> People </li>
-          <li> Planets</li>
-          <li> SpaceShip</li>
-        </ul>
+    <>
+      <div className="topnav">
+        <div className="Logo"></div>
+        <div className="nav">
+          <ul>
+            <li> People </li>
+            <li> Planets</li>
+            <li> SpaceShip</li>
+          </ul>
+        </div>
       </div>
-    </div>
+
+    
+      <People/>
+    </>
   );
 }
 
---
+++ src/components/People.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../App.css";
+
+function People () {
+  const [data, setData] = useState([]);
+  const [people, setPeople] = useState([]);
+  const [query, setQuery] = useState(1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(
+        `https://swapi.dev/api/people/?page=${query}`
+      );
+      setData(res.data);
+      setPeople(res.data.results);
+    };
+    fetchData();
+  }, [query]);
+
+  console.log(data);
+  console.log(people);
+  console.log(query);
+
+  return (
+    <>
+    
+      <ul>
+        {people.map(item => (
+          <li key={item.name}>{item.name}</li>
+        ))}
+      </ul>
+      <button
+        type="text"
+        value={query}
+        onClick={event => {
+          if (query > 1) {
+            setQuery(query - 1);
+          }
+        }}
+      >
+        Previous
+      </button>
+      <button
+        type="text"
+        value={query}
+        onClick={event => {
+          if (query < 9) {
+            setQuery(query + 1);
+          }
+        }}
+      >
+        Next
+      </button>
+    </>
+  );
+}
+
+export default People;
